5994b030b042eea3135c2362c78f2064b6b59f9a
Anton Kuznetsov <isnifer@gmail.com>
2014-07-25 18:34:13 +0400

    Search & Filter

--- assets/js/app.min.js
+++ assets/js/app.min.js
@@ -1,66 +1,70 @@
 /** @jsx React.DOM */
-var $main = document.querySelector('.main'); 
+;(function (window, React, $, undefined) {
 
-var ListBox = React.createClass({
+  var $main = document.querySelector('.main'); 
 
-  getInitialState: function () {
-    return {data: []};
-  },
+  var ListBox = React.createClass({
 
-  loadData: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
+    getInitialState: function () {
+      return {data: []};
+    },
 
-  componentDidMount: function () {
-    this.loadData();
-  },
+    loadData: function() {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
 
-  render: function() {
-    return (
-      <div className="listbox">
-        <ListPeople data={this.state.data} />
-        <button onClick={this.loadData}>Подгрузить данные</button>
-      </div>
-    );
-  }
+    componentDidMount: function () {
+      this.loadData();
+    },
 
-});
-
-var ListPeople = React.createClass({
-  render: function () {
-    var people = this.props.data.map(function (man) {
+    render: function() {
       return (
-        <ListItem name={man.name} age={man.age} />
+        <div className="listbox">
+          <ListPeople data={this.state.data} />
+          <button onClick={this.loadData}>Подгрузить данные</button>
+        </div>
       );
-    });
+    }
 
-    return (
-      <div className="listbox__list">
-        {people}
-      </div>
-    );
-  }
-});
+  });
 
-var ListItem = React.createClass({
+  var ListPeople = React.createClass({
+    render: function () {
+      var people = this.props.data.map(function (man) {
+        return (
+          <ListItem name={man.name} age={man.age} />
+        );
+      });
 
-  render: function () {
-    return (
-      <div className="listbox__item">
-        {this.props.name}: {this.props.age}
-      </div>
-    );
-  }
+      return (
+        <div className="listbox__list">
+          {people}
+        </div>
+      );
+    }
+  });
 
-});
+  var ListItem = React.createClass({
 
-React.renderComponent(<ListBox url="data.json" />, $main);
+    render: function () {
+      return (
+        <div className="listbox__item">
+          {this.props.name}: {this.props.age}
+        </div>
+      );
+    }
+
+  });
+
+  React.renderComponent(<ListBox url="data.json" />, $main);
+
+}(window, window.React, window.jQuery));
